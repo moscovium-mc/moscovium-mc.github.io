8b3a0c4bedfed211c3c6c42eec8433f1500cdc1f
moscovium-mc <155406798+moscovium-mc@users.noreply.github.com>
2026-02-06 04:22:17 +0000

    Deploying to gh-pages from @ moscovium-mc/moscovium-mc.github.io@d58f110c557f309d997b17e797e0820467489a75 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -549,11 +549,6 @@ ninja.data = [{
           description: "The world&#39;s largest hacker conference",
           section: "Teachings",handler: () => {
               window.location.href = "/teachings/defcon_2026/";
-            },},{id: "teachings-introduction-to-machine-learning",
-          title: 'Introduction to Machine Learning',
-          description: "This course provides an introduction to machine learning concepts, algorithms, and applications. Students will learn about supervised and unsupervised learning, model evaluation, and practical implementations.",
-          section: "Teachings",handler: () => {
-              window.location.href = "/teachings/introduction-to-machine-learning/";
             },},{
         id: 'social-email',
         title: 'email',
